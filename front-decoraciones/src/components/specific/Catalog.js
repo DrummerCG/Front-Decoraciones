0f37a3f40c9207291c804c60058a85ef81083282
Santiago Quiroz upegui <santiqupgui@gmail.com>
2024-04-25 15:43:04 -0500

    consulta de productos

--- front-decoraciones/src/components/specific/Catalog.js
+++ front-decoraciones/src/components/specific/Catalog.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Catalog = () => {
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        // Realiza la solicitud al montar el componente
+        axios.get("http://localhost:3001/productos")
+          .then(response => {
+            setProductos(response.data); // Actualiza el estado con los datos obtenidos
+          })
+          .catch(error => {
+            console.error("Error al obtener los productos:", error);
+          });
+      }, []); // El segundo argumento vacío asegura que se ejecute solo una vez al montar el componente
+
+
     return(
-        <p>catalogo</p>
+        <>
+            <p>catalogo</p>
+            {productos.map(producto => <p>{producto.nombre}</p>)}
+        </>
+        
     )
 }
 
